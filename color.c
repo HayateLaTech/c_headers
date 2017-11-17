ec947c21f735d5d0d9e55b851804dc115fb5f76a
HayateLaTech <broladin@gmail.com>
2017-11-17 15:58:40 +0100

    Create color.c

---
+++ color.c
@@ -0,0 +1,21 @@
+void red() {
+  printf("\x1b[31m");
+}
+void blue() {
+  printf("\x1b[34m");
+}
+void green() {
+  printf("\x1b[32m");
+}
+void magenta() {
+  printf("\x1b[35m");
+}
+void cyan() {
+  printf("\x1b[36m");
+}
+void yellow() {
+  printf("\x1b[33m");
+}
+void reset() {
+  printf("\x1b[0m");
+}
