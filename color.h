150ef6e7de21f2d4d7ef7122edf216cce543beee
HayateLaTech <broladin@gmail.com>
2017-11-17 15:58:30 +0100

    Create color.h

---
+++ color.h
@@ -0,0 +1,13 @@
+#ifndef COLOR_H_INCLUDED
+#define COLOR_H_INCLUDED
+
+void red(void);
+void blue(void);
+void green(void);
+void magenta(void);
+void cyan(void);
+void yellow(void);
+void reset(void);
+
+
+#endif // COLOR_H_INCLUDED
